94a51381fd2162732171b9df75237d18a3f623ab
Supergmam02 <griffith.a.disabato@gmail.com>
2025-10-30 14:21:18 -0400

    refactor main.py to remeber past  user prompts for Gemini API calls

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 import sys
 
 def main():
@@ -8,12 +9,16 @@ def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
-    print(f"sys.argv: {sys.argv}")
     if len(sys.argv) == 1:
         print("Please give a prompt")
         sys.exit(1)
+    user_prompt = sys.argv[1:]
+    messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+]
     response = client.models.generate_content(
-        model ='gemini-2.0-flash-001', contents = sys.argv
+        model ='gemini-2.0-flash-001',
+        contents = messages
 )
     print(response.text)
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
