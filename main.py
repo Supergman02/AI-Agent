f915264e264ff8def82675190464a8257dcd62f9
Supergmam02 <griffith.a.disabato@gmail.com>
2025-10-30 14:16:34 -0400

    updated main.py to take prompts from the commandline to ask Gemini

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import sys
 
 def main():
     print("Hello from ai-agent!")
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
+    print(f"sys.argv: {sys.argv}")
+    if len(sys.argv) == 1:
+        print("Please give a prompt")
+        sys.exit(1)
     response = client.models.generate_content(
-        model ='gemini-2.0-flash-001', contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        model ='gemini-2.0-flash-001', contents = sys.argv
 )
     print(response.text)
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
