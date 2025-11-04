878b516098274adfb333185aeb53a2ca663081c7
Supergmam02 <griffith.a.disabato@gmail.com>
2025-11-04 11:40:44 -0500

    refactor get_file_content and get_files_info functions for improved readability and error handling

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,17 +1,21 @@
 import os
-from config import MAX_CHARS    
+from config import MAX_CHARS
+
+
 def get_file_content(working_directory, file_path):
+    abs_working_dir = os.path.abspath(working_directory)
+    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
+    if not abs_file_path.startswith(abs_working_dir):
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    if not os.path.isfile(abs_file_path):
+        return f'Error: File not found or is not a regular file: "{file_path}"'
     try:
-        full_path = os.path.join(working_directory, file_path)
-        if os.path.abspath(working_directory) not in os.path.abspath(full_path):
-                return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
-        if not os.path.isfile(full_path):
-                return f'Error: File not found or is not a regular file: "{file_path}"'
-        with open(full_path, "r") as f:
-            print("Reading file:", full_path)
-            file_content_string = f.read()
-            print(f"File content length: {len(file_content_string)} characters")
-            return_string = file_content_string[:MAX_CHARS] + f'\n[...File "{file_path}" truncated at 10000 characters]' if len(file_content_string) > MAX_CHARS else file_content_string
-            return return_string
+        with open(abs_file_path, "r") as f:
+            content = f.read(MAX_CHARS)
+            if os.path.getsize(abs_file_path) > MAX_CHARS:
+                content += (
+                    f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+                )
+        return content
     except Exception as e:
-        return f'Error: {str(e)}'
+        return f'Error reading file "{file_path}": {e}'
