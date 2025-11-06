dc26608b6fac915c53e59939e6e6d7aeee5ec9cd
Supergmam02 <griffith.a.disabato@gmail.com>
2025-11-06 13:58:28 -0500

    implement call_function to handle dynamic function calls with verbose output; update main to utilize call_function and manage function responses

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ from google.genai import types
 import sys
 
 from prompts import system_prompt
-from call_function import available_functions
+from call_function import available_functions, call_function
 
 def main():
-    print("Hello from ai-agent!")
     load_dotenv()
+    verbose ="--verbose" in sys.argv 
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
     if len(sys.argv) == 1:
@@ -27,7 +27,7 @@ def main():
         ),
 )
     print(response.text)
-    if "--verbose" in sys.argv:
+    if verbose:
         print(f"User prompt: {user_prompt}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
@@ -35,8 +35,21 @@ def main():
     if not response.function_calls:
         return response.text
 
+    function_responses = []
     for function_call_part in response.function_calls:
-        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+        function_call_result = call_function(function_call_part, verbose)
+        if (
+            not function_call_result.parts
+            or not function_call_result.parts[0].function_response
+        ):
+            raise Exception("empty function call result")
+        if verbose:
+            print(f"-> {function_call_result.parts[0].function_response.response}")
+        function_responses.append(function_call_result.parts[0])
+
+    if not function_responses:
+        raise Exception("no function responses generated, exiting.")
+
 
 if __name__ == "__main__":
     main()
